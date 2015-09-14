6d6b3ecf4761ae0c8f0a0516ae9289d15469a161
asfagaragan <ashleyfa@hawaii.edu>
2015-09-14 10:46:41 -1000

    In class assignment - DOM events : onclick, onmouseenter, onmouseleave

---
+++ IC/w4c1/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by asfagaragan on 9/14/15.
+ */
+function changeText(id){
+    id.innerHTML = "Hello!";
+}
+
+function changeTextColor(id){
+    document.getElementById("text1").style.color = "red";
+}
+
+function changeTextColorBack(id){
+    document.getElementById("text1").style.color = "black";
+}
+
+function changeSidebar(id){
+    id.innerHTML = "Here I am! But where did the button go?! :o";
+}
+
