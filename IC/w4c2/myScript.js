ed8f1a62dc36611d2acd933604d1c62954d16ad6
asfagaragan <ashleyfa@hawaii.edu>
2015-09-16 21:07:27 -1000

    In class assignment 09/16 - validation fixed, stored info into table on process.html but could not figure out how to store all submitted forms and check if there isn't any duplicates

--- IC/w4c2/myScript.js
+++ IC/w4c2/myScript.js
@@ -1,19 +1,43 @@
 window.onload = codeAddress;
 
-function AddData(){
+function AddData(form_data){
     var table = document.getElementById('list');
     var row = document.createElement('tr');
     var td1 = document.createElement('td');
     var td2 = document.createElement('td');
     var td3 = document.createElement('td');
 
-    td1.innerHTML = document.getElementById('username').value;
-    td2.innerHTML = document.getElementById('address').value;
-    td3.innerHTML = document.getElementById('phone').value;
+    var username;
+    var address;
+    var phone;
+
+    var i;
+    var searchString = window.location.search.substring(1);
+    var params = searchString.split("&");
+
+    var val;
+    for (i=0;i<params.length;i++) {
+        if(params[i].substring(0,4) == "name"){
+            username = params[i].substring(5, params[i].length);
+
+        }
+        else if(params[i].substring(0,7) == "address"){
+            address = params[i].substring(8, params[i].length);
+        }
+        else if(params[i].substring(0,5) == "phone"){
+            phone = params[i].substring(6, params[i].length);
+        }
+    }
+
+
+    td1.innerHTML = username;
+    td2.innerHTML = address;
+    td3.innerHTML = phone;
     row.appendChild(td1);
     row.appendChild(td2);
     row.appendChild(td3);
-    table.children[0].appendChild(row);
+    console.log(table.children[0].appendChild(row));
+
 }
 
 
@@ -30,7 +54,8 @@ function changeText(id){
 }
 
 function codeAddress(){
-    document.getElementById("demo").innerHTML = window.location.search;
+    var form_data = window.location.search.substring(1);
+    AddData(form_data);
 }
 
 function validateForm() {
@@ -49,4 +74,5 @@ function validateForm() {
         alert("address must be filled out");
         return false;
     }
+    return true;
 }
