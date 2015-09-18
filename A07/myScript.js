177812190744cfb9e1bbd47bf43d899057518cbf
asfagaragan <ashleyfa@hawaii.edu>
2015-09-17 20:26:03 -1000

    A07  - tried to play with it

--- A07/myScript.js
+++ A07/myScript.js
@@ -1,32 +1,24 @@
 function validateForm() {
+    var i;
     var x = document.forms["myForm"]["username"].value;
     if (x == null || x == "") {
+        document.getElementById('username').style.background = "red";
         alert("username must be filled out");
-        document.getElementById("username").select().style.borderColor = "red";
         return false;
     }
-    else{
-        document.getElementById('username').style.borderColor = "green";
-    }
+
     var y = document.forms["myForm"]["email"].value;
-    if(y = null || y == ""){
+    if (y == null || y == "") {
+        document.getElementById('email').style.background = "red";
         alert("email must be filled out");
         return false;
     }
-    var z = document.forms["myForm"]["password"].value;
-    if(z = null || z == ""){
-        alert("password must be filled out");
-        return false;
+
+    switch(i){
+        case 1:
+            alert.getElement
     }
-    var w = document.forms["myForm"]["confirm"].value;
-    if(w = null || w == ""){
-        alert("password must be filled out");
-        return false;
-    }
-    else if((z.localeCompare(w)) == -1){
-        alert("password does not match");
-        return false;
-    }
+
     return true;
 }
 
